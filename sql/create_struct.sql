000d998ee840a360b472381cac602bdc05428ea8
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-04-12 23:30:21 -0300

    FEAT(create_struct): Criando sql da tabela dados_maquinas

--- sql/create_struct.sql
+++ sql/create_struct.sql
@@ -4,7 +4,13 @@ USE dados_algas;
 CREATE TABLE IF NOT EXISTS dados (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	value FLOAT NOT NULL,
-	memory_used FLOAT NOT NULL,
+	time_used FLOAT NOT NULL,
+	datetime_insert DATETIME NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS dados_maquinas (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name STRING NOT NULL,
 	time_used FLOAT NOT NULL,
 	datetime_insert DATETIME NOT NULL
 )

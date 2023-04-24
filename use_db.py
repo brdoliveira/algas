13d5b16f9eeac14ca9d188ff00a1aa7241b889ee
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-04-23 23:55:58 -0300

    FEAT(use_db): Buscando dados das maquinas

--- use_db.py
+++ use_db.py
@@ -31,14 +31,14 @@ def create_table() -> None:
                     value FLOAT NOT NULL,
                     time_used FLOAT NOT NULL,
                     datetime_insert DATETIME NOT NULL
-                )
+                );
 
                 CREATE TABLE IF NOT EXISTS dados_maquinas (
                     id INT AUTO_INCREMENT PRIMARY KEY,
-                    name STRING NOT NULL,
+                    name VARCHAR(255),
                     time_used FLOAT NOT NULL,
                     datetime_insert DATETIME NOT NULL
-                )
+                );
             """
 
             mycursor.execute(create_table)
@@ -79,12 +79,24 @@ def insert_table(value : float, time_used: float) -> None:
             mydb.close()
 
 def insert_spend_process(name: str,time_used: float) -> None:
+    """
+    Função responsável por inserir registro no banco de dados.
+
+    Args: 
+        name (str): Nome do servidor que será adicionado no banco de dados
+        time_used (float): Tempo gasto na execução
+    Returns:
+        none
+
+    Errors:
+        mysql.connector.Error: Erro com a conexão com o banco de dados
+    """
     mydb.connect()
     try:
         if mydb.is_connected():
             mycursor = mydb.cursor()
 
-            sql_query = f'INSERT INTO {database}.dados_maquinas(id,name,time_used,datetime_insert) VALUES (null,{name},{time_used},now())'
+            sql_query = f"INSERT INTO {database}.dados_maquinas(id,name,time_used,datetime_insert) VALUES (null,'{name}',{time_used},now())"
 
             mycursor.execute(sql_query)
             mydb.commit()
@@ -114,6 +126,37 @@ def get_inserts(columns : str = "*") -> list:
             mycursor = mydb.cursor()
 
             sql_query = f'SELECT {columns} FROM {database}.dados'
+            mycursor.execute(sql_query)
+
+            result = mycursor.fetchall()
+            mydb.commit()
+
+            return result
+    except mysql.connector.Error as e:
+        print("Erro ao conectar com o MySQL: ", e)
+    finally:
+        if mydb.is_connected():
+            mycursor.close()
+            mydb.close()
+
+def get_inserts_data_machine() -> None:
+    """
+    Função responsável por buscar os valores inseridos na tabela dados.
+
+    Args:
+        columns (str, optional) : Colunas que vão ser retornados, o padrão é retornar todas as colunas
+    Returns:
+        list: Irá retornar os valores adicionados na tabela dados
+
+    Errors:
+        mysql.connector.Error: Erro com a conexão com o banco de dados
+    """
+    mydb.connect()
+    try:
+        if mydb.is_connected():
+            mycursor = mydb.cursor()
+
+            sql_query = f'SELECT name, avg(time_used) as media FROM {database}.dados_maquinas GROUP BY name'
             mycursor.execute(sql_query)
 
             result = mycursor.fetchall()

c4f7a02d804d07f234a0ccada97dff3dfebd45aa
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-04-23 23:55:31 -0300

    FEAT(show_plots): Plotando grafico de perfomance

--- show_plots.py
+++ show_plots.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from use_db import *
 
-def show_plot_value() -> None:
+def show_plot_data() -> None:
     """
     Função responsável por criar um gráfico com o valores que estão no banco de dados e plotá-lô.
 
@@ -10,34 +10,60 @@ def show_plot_value() -> None:
     Returns:
         none
     """
-    result : list = get_inserts('id,value,memory_used,time_used')
+    result : list = get_inserts('id,value,time_used')
     list_id : list = []
     list_noise_value : list = []
-    list_memory_used : list = []
     list_time_used: list = []
 
-    for id, value,memory,time in result:
+    for id, value,time in result:
         list_id.append(id)
         list_noise_value.append(value)
-        list_memory_used.append(memory)
         list_time_used.append(time)
 
-    graph1 = plt.subplot(212)
+    graph1 = plt.subplot(121)
     graph1.set_xlabel("Nº de registros")
     graph1.set_ylabel("Ruido(dBA)")
     graph1.plot(list_id,list_noise_value,'r')
     graph1.set_title("Correlação entre o ruido e o tempo")
     graph1.axhline(y = 65, color = 'black', linestyle = '-')
 
-    graph2 = plt.subplot(221)
+    graph2 = plt.subplot(122)
     graph2.set_ylabel("Tempo (ms)")
+    graph2.set_xlabel("Nº de registros")
     graph2.plot(list_id,list_time_used,'black')
     graph2.set_title("Tempo gasto na execução")
 
-    graph3 = plt.subplot(222)
-    graph3.set_ylabel("Memória(kb)")
-    graph3.plot(list_id,list_memory_used,'black')
-    graph3.set_title("Gasto na execução")
     plt.show()
 
-show_plot_value()
+def show_plot_machine() -> None:
+    """
+    Função responsável por criar um gráfico com o valores que estão no banco de dados e plotá-lô.
+
+    Args:
+        none
+    Returns:
+        none
+    """
+    result : list = get_inserts_data_machine()
+    list_value_machine : list = []
+    list_name_machine : list = []
+
+    print(result)
+
+    for name, value in result:
+       list_name_machine.append(name)
+       list_value_machine.append(value)
+    
+    plt.xlabel("Nome das maquinas")
+    plt.ylabel("Tempo gasto pelas maquinas")
+    plt.plot(
+        list_name_machine,
+        list_value_machine,
+        marker="o"
+    )
+    plt.title("Tempo gasto por maquina")
+
+    plt.show()
+
+show_plot_data()
+show_plot_machine()

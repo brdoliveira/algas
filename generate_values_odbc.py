5f3915e975559453239e8c5ee90c979cc6f10afb
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-04-25 20:18:12 -0300

    FEAT(generate_values_odbc): Criando geração de dados e salvando na nuvem

---
+++ generate_values_odbc.py
@@ -0,0 +1,62 @@
+import time
+import pandas as pd
+from use_odbc import *
+
+def get_dBA() -> None:
+    """
+    Função responsável por pegar apenas a coluna 'dBA' depois salvar em um arquivo .csv separado.
+
+    Args:
+        none
+    Returns:
+        none
+
+    Notes:
+        dBA: dB representa decibéis e é uma unidade de medida de som. Esta unidade mede a intensidade de um som
+        ou a força de um sinal. 
+    See: https://www.ehow.com.br/diferencas-entre-dba-dbc-info_41877/
+            https://iot.fvh.fi/opendata/noise/
+    """
+    data = pd.read_csv('./data/LAeq-2020-all.csv')
+    getDBA = data['dBA']
+    getDBA.to_csv('./data/dBA.csv')
+
+def insert_dBA_BD(name) -> None:
+    """
+    Função responsável por gerar um valor aleatório e salvar no banco de dados.
+
+    Args:
+        none
+    Returns:
+        none
+    """
+    dataDBA = pd.read_csv('./data/dBA.csv')
+
+    n_samples : int = 10
+
+    while True:
+
+        start_time_for = time.time()
+        for _ in range(10):
+            start_time = time.time()
+            
+            sample = dataDBA['dBA'].sample(n_samples).mean()
+
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            elapsed_time = round(elapsed_time, 4)
+
+            insert_table_odbc(
+                value = float(sample),
+                time_used= float(elapsed_time) 
+            )
+  
+        end_time_for = time.time()
+        elapsed_time_for = end_time_for - start_time_for
+        elapsed_time_for = round(elapsed_time_for,4)
+
+        insert_spend_process_odbc(name,elapsed_time_for)
+
+print('Insira o nome da Maquina:')
+nome_maquina = input()
+insert_dBA_BD(nome_maquina)
